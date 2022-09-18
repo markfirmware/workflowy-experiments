1c167fa78359c3ff2453745d458e1560924488b3
markfirmware <markfirmware@users.noreply.github.com>
2022-09-17 21:26:04 -0400

    pass all events to saved listener

--- dates-updater.user.js
+++ dates-updater.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WorkflowyDatesUpdater
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  Update dates and trailing spaces
 // @author       Mark E Kendrat
 // @match        https://workflowy.com
@@ -13,49 +13,42 @@
 
 (() => {
     'use strict'
-    const script_name = 'WorkflowyDates'
+    const script_name = 'WorkflowyDatesUpdater'
     const improvedDatesAndSpaces = original => {
         var improved = original
         // remove all spaces at end of line
         improved = improved.replace(/ +$/, '')
-        // put 0 in front of single digit date
+        // put 0 in front of all single digit dates
         improved = improved.replaceAll(/, (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (\d), /g, ', $1 0$2, ')
-        // put space in front of single digit hour
+        // put space in front of all single digit hours
         improved = improved.replaceAll(/ at (\d):(\d\d)(am|pm)/g, ' at  $1:$2$3')
         return improved
     }
-    var listener
     const saved_wfeventlistener = window.WFEventListener
+    var is_active = false, last_focused_item = null
     window.WFEventListener = event => {
         try {
-            listener?.(event)
             saved_wfeventlistener?.(event)
-        } catch (exception) {
-            console.log(script_name, exception)
-        }
-    }
-    listener = event => {
-        if (event == 'documentReady') {
-            var last_focused_item = window.WF.focusedItem()
-            var is_listener_active = false
-            listener = event => {
-                if (is_listener_active) {
-                    return
-                }
-                is_listener_active = true
-                if (last_focused_item &&
-                    (last_focused_item.getId() != window.WF.focusedItem()?.getId() ||
-                     event == 'operation--bulk_create')) {
+            if (is_active) {
+                console.log(script_name, 'ignored nested event', event)
+                return
+            } else {
+                is_active = true
+                if (last_focused_item && (last_focused_item.getId() != window.WF.focusedItem()?.getId()
+                                          || event == 'operation--bulk_create')) {
                     var original = last_focused_item.getName()
                     var improved = improvedDatesAndSpaces(original)
                     if (improved != original) {
                         window.WF.setItemName(last_focused_item, improved)
-                        console.log(script_name, 'item updated', improved)
+                        console.log(script_name, event, 'item updated', improved)
                     }
                 }
                 last_focused_item = window.WF.focusedItem()
-                is_listener_active = false
+                is_active = false
             }
+        } catch (exception) {
+            console.log(script_name, 'WFEventListener', exception)
         }
     }
+    console.log(script_name, 'userscript loaded')
 })()
