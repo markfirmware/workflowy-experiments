72c524b65c80b445ebd1b690ce0bdf52b88dc7cd
markfirmware <markfirmware@users.noreply.github.com>
2022-09-19 16:47:38 -0400

    add WorkflowyTabClient.user.js

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ In order to not interfere with data entry, the item will not be updated until th
 1. Install [Tampermonkey](https://www.tampermonkey.net/)
 2. Install this script via this link: [dates-updater.user.js](https://github.com/markfirmware/workflowy-experiments/raw/master/dates-updater.user.js)
 
+
+# WorkflowyTabClient.user.js
+
+This will open a new tab/window (you must enable pop-ups from workflowy.com) and will print in the console log of the new window any events from the workflowy tab. This is accomplished by using a web browser BroadcastChannel.
+
+1. Install [Tampermonkey](https://www.tampermonkey.net/)
+2. Install this script via this link: [WorkflowyTabClient.user.js](https://github.com/markfirmware/workflowy-experiments/raw/master/WorkflowyTabClient.user.js)
+
---
+++ WorkflowyTabClient.user.js
@@ -0,0 +1,88 @@
+// ==UserScript==
+// @name         WorkflowyTabClient
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Demo browser tab receiving Workflowy events
+// @author       Mark E Kendrat
+// @match        https://workflowy.com
+// @match        https://beta.workflowy.com
+// @match        https://dev.workflowy.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=workflowy.com
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    'use strict'
+    const script_name = 'WorkflowyTabClient'
+
+    function program(supplied_channel_name, openClientWindow) {
+        const trap = f => {
+            return (...args) => {
+                try {
+                    return f(...args)
+                } catch (e) {
+                    console.log('trap', script_name, e)
+                }
+            }
+        }
+        const Broadcast = (() => {
+            var rx, tx
+            const createChannel = name => {
+                tx = new BroadcastChannel(name)
+                rx = new BroadcastChannel(name)
+                rx.addEventListener('message', trap(message => console.log(JSON.stringify(message.data))))
+            }
+            const post = message => tx.postMessage(message)
+            return { createChannel, post }
+        })()
+        if (supplied_channel_name) {
+            Broadcast.createChannel(supplied_channel_name)
+            Broadcast.post({ tag: 'ClientActive' })
+        } else {
+            if (window.WFEventListener === undefined) {
+                const channel_name = script_name + '.' + Date.now()
+                Broadcast.createChannel(channel_name)
+                openClientWindow(channel_name)
+                Broadcast.post({ tag: 'ServerActive'})
+                window.WFEventListener = trap(eventName => Broadcast.post({ tag: 'WfEvent', eventName }))
+                console.log('window.WFEventListener set by', script_name)
+            } else {
+                console.log('window.WFEventListener unavailable for', script_name)
+            }
+        }
+    }
+
+    const openClientWindow = channel_name => {
+        const page = `
+          <html>
+              <title>${channel_name}</title>
+              <head>
+                  <script type="module">
+                      try {
+                          (${program})("${channel_name}")
+                      } catch (exception) {
+                          console.log("exception", script_name, "${channel_name}", exception)
+                      }
+                  </script>
+              </head>
+              <body>
+                  <div>Please open the javascript console with control-shift-i to see received workflowy events.</div>
+                  <div>These events are passed through a web browser BroadcastChannel.</div>
+                  <div>The channel can also be used to send requests to the workflowy tab and receive responses.</div>
+              </body>
+          </html>`
+      const w = window.open()
+      if (w) {
+          const d = w.document
+          d.open()
+          d.write(page)
+          d.close()
+      } else {
+          console.log(script_name, 'could not open window/tab - peehaps enable pop-ups from workflowy.com?')
+      }
+    }
+
+    program('', openClientWindow)
+
+    console.log(script_name, 'userscript loaded')
+})()
