9f500cb78d9233f325ce0f541311aebaf091d268
markfirmware <markfirmware@users.noreply.github.com>
2022-12-07 18:35:13 +0000

    Update workflowy-project-tag.user.js

--- workflowy-project-tag.user.js
+++ workflowy-project-tag.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Workflowy Project Tag
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Set completed-visible within a project. Projects are tagged with #project.
 // @author       Mark E Kendrat
 // @match        https://*.workflowy.com/
@@ -11,9 +11,9 @@
 
 (function() {
     'use strict'
-    const scriptName = 'Workflowy Project Tag'
+    const scriptName = GM_info.script.name
     const projectTag = '#project'
-    var lastIsInsideProject = false
+    var prevIsInsideProject = false
     function crumbs(cs = [window.WF.currentItem()]) {
         var p = cs[0].getParent()
         return p ? crumbs([p, ...cs]) : cs
@@ -21,31 +21,22 @@
     function tags(item) {
         return item.getTagManager().getTagCounts().getTagList().map(x => x.tag)
     }
-    function isItemInsideProject(item) {
-        return crumbs().some(item => tags(item).includes(projectTag))
-    }
-    function setVisibility() {
-        const item = window.WF.currentItem()
-        const isInside = isItemInsideProject(item)
-        if (isInside != lastIsInsideProject) {
-            lastIsInsideProject = isInside
-            console.log('transition', isInside)
-            if (isInside != window.WF.completedVisible()) {
-                window.WF.toggleCompletedVisible()
-                console.log('toggled')
-            }
-        }
-    }
-    var savedWFEventListener = window.WFEventListener
+    const savedWFEventListener = window.WFEventListener
     window.WFEventListener = function(event) {
         savedWFEventListener?.()
         try {
             if (event == 'documentReady' || event == 'locationChanged' || event == 'zoomedOut') {
-                setVisibility()
+                const isInsideProject = crumbs().some(item => tags(item).includes(projectTag))
+                if (isInsideProject != prevIsInsideProject) {
+                    prevIsInsideProject = isInsideProject
+                    if (isInsideProject != window.WF.completedVisible()) {
+                        window.WF.toggleCompletedVisible()
+                    }
+                }
             }
         } catch (e) {
             console.log(scriptName, e)
         }
     }
-    console.log(scriptName, 'loaded')
+    console.log(scriptName, 'userscript loaded')
 })()
